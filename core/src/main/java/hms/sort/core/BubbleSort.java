9fc77fcfa19833f7fad15028b308a5e2b0547854
Kushini Hathurusinghe <kushinih@hsenidmobile.com>
2019-08-12 17:28:56 +0530

    Add copyright

--- core/src/main/java/hms/sort/core/BubbleSort.java
+++ core/src/main/java/hms/sort/core/BubbleSort.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * (C) Copyright 2010-2019 hSenid Mobile Solutions (Pvt) Limited.
+ * All Rights Reserved.
+ *
+ * These materials are unpublished, proprietary, confidential source code of
+ * hSenid Mobile Solutions (Pvt) Limited and constitute a TRADE SECRET
+ * of hSenid Mobile Solutions (Pvt) Limited.
+ *
+ * hSenid Mobile Solutions (Pvt) Limited retains all title to and intellectual
+ * property rights in these materials.
+ ******************************************************************************/
+
 package hms.sort.core;
 
 public class BubbleSort {
